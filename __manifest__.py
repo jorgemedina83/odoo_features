6c7930b2aab39584b81d127c05377733060e89da
jorge.medina <desarrollo.sacsa@gruposacsa.com.mx>
2024-01-27 13:09:42 -0700

    Estructura Modulo Odoo

---
+++ __manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Odoo SACSA Caracteristicas",
+
+    'summary': """
+        Módulo General Odoo operación de SACSA
+        """,
+    'description': """
+      """,
+    'author': "Grupo SACSA",
+    'website': "https://www.gruposacsa.com.mx/",
+
+    'category': 'Invoicing',
+    'version': '1.0',
+
+    # any module necessary for this one to work correctly
+    'depends': [],
+
+    # always loaded
+    'data': [        
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+    ],
+}
